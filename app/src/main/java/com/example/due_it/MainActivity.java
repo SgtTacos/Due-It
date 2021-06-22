7e3058360402d048c33938f1e9d7f9cbfde97e7f
Sgt Tacos <tsosie.everett@gmail.com>
2021-06-22 15:33:38 -0600

    Fixed SharedPreference object attempting to cast into a string.

--- app/src/main/java/com/example/due_it/MainActivity.java
+++ app/src/main/java/com/example/due_it/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private static SharedPreferences sp;
     public String sec_Token;
 
-    /** This is the Main Activity Class.
+    /* This is the Main Activity Class.
      * This class will command the app work.
      */
 
@@ -26,56 +26,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /** Loading the app, it will retrieve a security token from SharedPreferences
-         * If a Token is present, it will be shown on screen and copied to text
-         * If there is no token in SharedPreferences
-         * a hint requesting Token will be shown
+        /* Loading the app, it will retrieve a security token from SharedPreferences
+          If a Token is present, it will be shown on screen and copied to text
+          If there is no token in SharedPreferences
+          a hint requesting Token will be shown
          */
-        SharedPreferences sp = getApplicationContext().getSharedPreferences("MyToken", Context.MODE_PRIVATE);
         EditText hint1 = findViewById(R.id.editToken);
         if (sp == null) {
             hint1.setHint("Security Access Token Required");
         } else {
-            hint1.setHint((CharSequence) sp);
-            hint1.setText((CharSequence) sp);
+            sp = getApplicationContext().getSharedPreferences("MyToken", Context.MODE_PRIVATE);
+            //hint1.setHint((CharSequence) sp.getString("MyToken", sec_Token));
+            //hint1.setText((CharSequence) sp.getString("MyToken", sec_Token));
         }
     }
 
     public void secToken(View view) {
-        /** This method will run when SECURITY Button is clicked
-         * Supposedly user will press security only if a new Token is provided
-         * Anyways if there is already a previous existing token, it will be re-saved
+        /* This method will run when SECURITY Button is clicked
+          Supposedly user will press security only if a new Token is provided
+          Anyways if there is already a previous existing token, it will be re-saved
          */
         sec_Token = securityToken();
     }
 
     private String securityToken() {
-        /** This method is invoked by method secToken and it is in charge of
+        /* This method is invoked by method secToken and it is in charge of
          *  getting the text content from the EditView object and verifying
          *  that it is not null. If it is will show a toast message.
          */
-        EditText txtToken = findViewById(R.id.editToken);
-        if (txtToken == null) {
-            Toast.makeText(MainActivity.this, "Missing Token", Toast.LENGTH_SHORT).show();
-        }
+        sec_Token = findViewById(R.id.editToken).toString();
+        Toast.makeText(MainActivity.this, "Missing Token", Toast.LENGTH_SHORT).show();
+
         saveToken(sec_Token);
         return sec_Token;
     }
 
     public static void saveToken(String sec_token) {
-        /** This method is invoked by securityToken and it is in charge of
-         *  saving the Token in SharedPreferences for future uses
-         *  It shows a Toast message indicating Token was saved
+        /* This method is invoked by securityToken and it is in charge of
+           saving the Token in SharedPreferences for future uses
+           It shows a Toast message indicating Token was saved
          */
         SharedPreferences.Editor editor = sp.edit();
         editor.putString("MyToken", sec_token);
         editor.apply();
+
         Toast.makeText((Context) sp,"Token was Saved", Toast.LENGTH_LONG).show();
     }
 
     void resultsResponse(List<String> due_assignments) {
-        /** This method exists to receive the results response,
-         *  translate it to the list and display the list
+        /* This method exists to receive the results response,
+           translate it to the list and display the list
          */
         Log.d("MainActivity", "Results: " + due_assignments);
         ArrayAdapter<String> ListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, due_assignments);
